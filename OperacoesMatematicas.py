60745325794d8647f99138ec0f40b6098db7dcd8
Bianca <bianca12brito@gmail.com>
2026-01-20 11:29:38 -0300

    Operações matemáticas básicas com Python

---
+++ OperacoesMatematicas.py
@@ -0,0 +1,11 @@
+
+soma= 10+10
+multiplicacao= 5 * 4
+divisao = 30 / 3
+potencia = 7 ** 2 
+
+print("soma",soma)
+print("multiplicacao",multiplicacao)
+print("divisao",divisao)
+print("potencia",potencia)
+
